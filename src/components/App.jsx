adab6c3061a53f5d35f6a5ebb5ff2973be4b2b66
Artyom <semperidem1989@gmail.com>
2024-04-22 21:53:55 +0500

    homwork3/verification

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 import Header from "./Header";
 import Footer from "./Footer";
 import Skills from "./Skills";
@@ -26,56 +27,89 @@ const App = () => {
 
     ]
 
-    const study = [
+    const [isShowStudy, showStudy] = React.useState(false);
+    const [firstData, setFirstData] = React.useState([
 
         {
             text: "Docker", 
+            level: 55,
+            id: uuidv4(),
          },
 
         {
             text: "Kubernetes",
+            level: 30,
+            id: uuidv4(),
         },
 
         {
             text: "Next",
+            level: 70,
+            id: uuidv4(),
         },
 
         {
             text: "Nest",
+            level: 28,
+            id: uuidv4(),
         },
 
         {
             text: "Vite", 
+            level: 62,
+            id: uuidv4(),
          },
 
         {
             text: "Vue",
+            level: 10,
+            id: uuidv4(),
         },
 
         {
             text: "Zustand",
+            level: 51,
+            id: uuidv4(),
         },
 
         {
             text: "Express",
+            level: 18,
+            id: uuidv4(),
         },
 
         {
             text: "Python",
+            level: 61,
+            id: uuidv4(),
         },
 
         {
             text: "Go",
+            level: 49,
+            id: uuidv4(),
         },
         
-    ]
+    ])
+
+    const [study, setStudy] = React.useState([...firstData])
+
+    function handleShowStudyClick() {
+        showStudy(!isShowStudy);
+      }
 
   return (
     <div className="App">
     <Header/>
     <main>
         <Skills data={skills}/>
-        <Study data={study}/>
+        <Study 
+        data={study}
+        firstData={firstData}
+        isShow={isShowStudy}
+        onShowStudy={handleShowStudyClick}
+        onSetStudy={setStudy}
+        />
     </main>
     <Footer/>
     </div>
--- src/components/Study.jsx
+++ src/components/Study.jsx
@@ -1,23 +1,138 @@
 import React from "react";
 
-export default function Study({data}) {
+export default function Study({data, firstData, isShow, onShowStudy, onSetStudy}) {
+
+const addCard = (e) => {
+    e.preventDefault();
+    const test = {text: newCompetence, level: newLevel};
+    onSetStudy([...data, test]);
+    setNewCompetence("");
+    setNewLevel("");
+}
+
+function deleteCard(id) {
+    onSetStudy(data.filter(item => item.id !== id));
+}
+
+const changeCompetence = (e) => {
+    setNewCompetence(e.target.value);
+}
+const changeLevel = (e) => {
+    setNewLevel(e.target.value);
+}
+
+const onCardMore = () => {
+    const moreCard = [];
+    for(let i = 0; i < firstData.length; ++i){
+        let card = firstData[i];
+        if(card.level >= 50){
+            moreCard.push(card);
+        }
+    }
+    onSetStudy([...moreCard])
+}
+const resetStudy = () => {
+    onSetStudy([...firstData]);
+}
+const onCardLess = () => {
+    const moreCard = [];
+
+    for(let i = 0; i < firstData.length; ++i){
+        let card = firstData[i];
+        if(card.level < 50){
+            moreCard.push(card);
+        }
+    }
+    onSetStudy([...moreCard])
+}
+ 
+const [newCompetence, setNewCompetence] = React.useState('');
+const [newLevel, setNewLevel] = React.useState('');
+
   return (
     <section className="study">
         <h2 className="skills__title study__title">Хочу изучить</h2>
-        <div className="study__cards">
 
-         {
-            data.map((item, i)=>{ 
-                 return (
+        <button className="study__show-button" 
+                type="button" 
+                onClick={onShowStudy}>{ isShow ? `Скрыть` : `Показать`}</button>
 
-                    <article className="study__element" key={i}>
-                        <span className="study__text">{item.text}</span>
-                    </article>
-                    
-                )
-            })
-         }
+            <div className={ isShow ? `study__container_opened` : `study__container`}>
 
+            <div className="study__buttons-and-form">
+                <div className="study__buttons-container">
+                    <button 
+                        className="study__show-more"
+                        onClick={onCardMore}
+                    >Изученно более 50%</button>
+                     <button 
+                        className="study__show-less"
+                        onClick={onCardLess}
+                     >Изученно менее 50%</button>
+                </div>
+                <div className="stady__competence-image" role="button" onClick={resetStudy}></div>
+                <div className="stady__add-form">
+                    <form action="#" name="add-form" className="stady__main" noValidate>
+
+                        <fieldset className="stady__name-competence">
+                            <input 
+                            type="text" 
+                            name="form-question" 
+                            className="study__form-input"
+                            placeholder="Название компетенции" 
+                            required value={newCompetence} 
+                            minLength="2" 
+                            maxLength="500" 
+                            id="name"
+                            onChange={(e)=>changeCompetence(e)}
+                            />
+                            <span className="study__input-error" id="name-error"></span>
+                        </fieldset>
+
+                        <fieldset className="stady__level-competence">
+                            <input 
+                            type="text" 
+                            name="form-question" 
+                            className="study__form-input"
+                            placeholder="Уровень освоения" 
+                            required value={newLevel} 
+                            minLength="2" 
+                            maxLength="500" 
+                            id="competence"
+                            onChange={(e)=>changeLevel(e)}
+                            />
+                            <span className="study__input-error" id="name-error"></span>
+                        </fieldset>
+
+                        <button 
+                        className="study__add-button" 
+                        type="submit" 
+                        onClick={(e)=>addCard(e)}
+                        >
+                            Добавить компетенцию
+                        </button>
+                    </form>
+                </div>
+            </div>
+
+            <div className="study__cards">
+
+                {
+                    data.map((item, i)=>{ 
+                        return (
+
+                            <article className="study__element" key={item.id}>
+                                <button className="study__close-button" onClick={() => deleteCard(item.id)}></button>
+                                <span className="study__text">{item.text}</span>
+                                <span className="study__text-description">Изученно</span>
+                                <span className="study__text-level">{item.level}%</span>
+                            </article>
+                            
+                        )
+                    })
+                }
+
+                </div>
             </div>
     </section>
   )
